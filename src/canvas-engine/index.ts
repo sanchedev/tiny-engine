9f37085124ce41fd19ee28884c7e459332b242d5
Sanchedev <sanchedev@outlook.com>
2026-02-20 17:56:57 -0500

    feat: Refactor imports to use index file for canvas engine

---
+++ src/canvas-engine/index.ts
@@ -0,0 +1,8 @@
+export { Game } from './game.js'
+export { GameConfig } from './game-config.js'
+export { Vector2 } from './classes/vector2.js'
+export { Scene } from './classes/scene.js'
+export { SceneManager } from './classes/scene-manager.js'
+export { Event } from './lib/event.js'
+
+export { Node } from './nodes/node.js'
--- src/game/main.ts
+++ src/game/main.ts
@@ -1,4 +1,4 @@
-import { Game } from '../canvas-engine/game.js'
+import { Game } from '../canvas-engine/index.js'
 
 const canvas = document.querySelector('canvas')!
 const ctx = canvas.getContext('2d')!
