909b98435e5b92c9e0884b503060e8de99af6bf6
Sanchedev <sanchedev@outlook.com>
2026-03-06 11:32:40 -0500

    feat: Add node as render component

--- packages/tiny-engine/src/core/scene.ts
+++ packages/tiny-engine/src/core/scene.ts
@@ -1,12 +1,19 @@
 import { jsx, type TinyComponent } from '../jsx-runtime.js'
+import { Node } from '../nodes/node.js'
 
-type RenderComponent = TinyComponent | Promise<TinyComponent>
+type RenderComponent =
+  | TinyComponent
+  | Promise<TinyComponent>
+  | Node
+  | Promise<Node>
 
 export class Scene {
   constructor(public render: () => RenderComponent) {}
 
   async load() {
     const node = await this.render()
+
+    if (node instanceof Node) return node
 
     return jsx(node, {})
   }
