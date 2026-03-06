bd74f214404eda596ccf2e4203d176f8e83b18d5
Sanchedev <sanchedev@outlook.com>
2026-03-06 15:46:34 -0500

    docs: Add docs for hooks

--- packages/tiny-engine/src/hooks/start.ts
+++ packages/tiny-engine/src/hooks/start.ts
@@ -1,6 +1,10 @@
 import type { TypeElements } from '../nodes/types.js'
 import { pushEffect } from './context.js'
 
+/**
+ * The **`useStart`** hooks call `fn` when the node starts.
+ * @param fn Function to call
+ */
 export function useStart<T extends keyof TypeElements = 'node'>(
   fn: (node: TypeElements[T]) => void,
 ) {
--- packages/tiny-engine/src/hooks/update.ts
+++ packages/tiny-engine/src/hooks/update.ts
@@ -1,6 +1,10 @@
 import type { TypeElements } from '../nodes/types.js'
 import { pushEffect } from './context.js'
 
+/**
+ * The **`useUpdate`** hooks call `fn` when the node updates.
+ * @param fn Function to call
+ */
 export function useUpdate<T extends keyof TypeElements = 'node'>(
   fn: (node: TypeElements[T], delta: number) => void,
 ) {
