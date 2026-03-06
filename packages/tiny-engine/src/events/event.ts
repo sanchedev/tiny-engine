1020caf18fab4a679cd0d2a0133d896f8617aba1
Sanchedev <sanchedev@outlook.com>
2026-03-06 15:51:28 -0500

    docs: Add docs for Event class

--- packages/tiny-engine/src/events/event.ts
+++ packages/tiny-engine/src/events/event.ts
@@ -1,9 +1,17 @@
 type Fun<T extends any[]> = (...args: T) => void
 
 export class Event<T extends any[]> {
+  /**
+   * The **`on`** method subscribes the `cb` to this `Event`.
+   * @param cb Callback
+   */
   on(cb: Fun<T>) {
     this.#list.push(cb)
   }
+  /**
+   * The **`off`** method unsubscribes the `cb` to this `Event`.
+   * @param cb Callback
+   */
   off(cb: Fun<T>) {
     const index = this.#list.indexOf(cb)
     if (index < 0) return
@@ -11,6 +19,10 @@ export class Event<T extends any[]> {
     this.#list.splice(index, 1)
   }
 
+  /**
+   * The **`emit`** method send the information to the subscribers of this `Event`.
+   * @param cb Callback
+   */
   emit(...params: T) {
     this.#list.forEach((cb) => cb(...params))
   }
