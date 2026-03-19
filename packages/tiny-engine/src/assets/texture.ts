8e305bf19ff0421d247b8437d6b8450ce659a5c3
Sanchedev <sanchedev@outlook.com>
2026-03-19 11:49:31 -0500

    feat: Add textures more flexibles

--- packages/tiny-engine/src/assets/texture.ts
+++ packages/tiny-engine/src/assets/texture.ts
@@ -1,6 +1,6 @@
 import { GameConfig } from '../core/game-config.js'
 import { TextureNotFoundError } from '../errors/assets.js'
-import type { Vector2 } from '../math/vector2.js'
+import { Vector2 } from '../math/vector2.js'
 
 export class Texture {
   /** The **`width`** of the texture. */
@@ -24,17 +24,30 @@ export class Texture {
     const width = options.size?.x ?? this.width
     const height = options.size?.y ?? this.height
 
+    const rWidth = options.resultSize?.x ?? width
+    const rHeight = options.resultSize?.y ?? height
+
+    const flipX = rWidth !== Math.abs(rWidth)
+    const flipY = rHeight !== Math.abs(rHeight)
+
+    const scaleX = flipX ? -1 : 1
+    const scaleY = flipY ? -1 : 1
+
+    const pos = options.position.toMultiplied(new Vector2(scaleX, scaleY))
+
+    GameConfig.ctx.scale(scaleX, scaleY)
     GameConfig.ctx.drawImage(
       this.image,
       options.margin?.x ?? 0,
       options.margin?.y ?? 0,
       width,
       height,
-      options.position.x,
-      options.position.y,
-      width,
-      height,
+      pos.x,
+      pos.y,
+      rWidth,
+      rHeight,
     )
+    GameConfig.ctx.scale(scaleX, scaleY)
   }
 }
 
@@ -43,6 +56,8 @@ interface TextureDrawOptions {
   position: Vector2
   /** Size of the texture */
   size?: Vector2 | undefined
+  /** Size of the result texture */
+  resultSize?: Vector2 | undefined
   /** Offset of the texture */
   margin?: Vector2 | undefined
 }
